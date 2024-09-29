d47bfe8654ebece54bf452a11cda1f6382014782
tahayk3 <tahayk3@gmail.com>
2024-09-29 11:13:15 -0600

    configurando wsgi

--- muebles_backend/muebles_backend/wsgi.py
+++ muebles_backend/muebles_backend/wsgi.py
@@ -1,8 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Cambia el módulo a 'muebles_backend.muebles_backend.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muebles_backend.muebles_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muebles_backend.settings')
 
 application = get_wsgi_application()
-
