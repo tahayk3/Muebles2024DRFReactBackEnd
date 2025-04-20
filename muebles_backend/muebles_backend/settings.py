9e2689832202f465461888bc688dbb970f19093c
mueblexnomadachapin <cristiantahay2024@gmail.com>
2025-04-19 21:11:08 -0600

    primer commit

--- muebles_backend/muebles_backend/settings.py
+++ muebles_backend/muebles_backend/settings.py
@@ -68,7 +68,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://muebles502xela.netlify.app",
     "https://66faeae64eece200f2d12ef7--muebles502xela.netlify.app",
     "https://muebles2024-drf-react-back-end.vercel.app",
-     "https://muebles2024-drf-react-back-qur9uowp9-tahayk3s-projects.vercel.app/",
+    "https://muebles2024-drf-react-back-qur9uowp9-tahayk3s-projects.vercel.app/",
 ]
 
 
